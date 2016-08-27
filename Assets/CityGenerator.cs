4c5b4cc6f1543b52dd0cf692d04e518230269a87
NoLevelCap <jemahsam@gmail.com>
2016-08-27 07:22:21 +0100

    Initial Init

---
+++ Assets/CityGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityGenerator : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject CopyTower;
+
+	public GameObject[] ChildrenTowers;
+
+	public float TowerHealth = 50;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void GenerateCity(){
+		for (int x = -1; x < 2; x+=2) {
+			for (int y = -1; y < 2; y+=2) {
+				GameObject Tower = Instantiate<GameObject> (CopyTower);
+
+				Tower.transform.SetParent (this.transform);
+				float Height = TowerHealth + (Random.value) * 5f;
+				Tower.transform.localScale = new Vector3 (0.38f, Height, 0.38f);
+				Tower.transform.localPosition = new Vector3 (x*0.25f, Height/2f, y*0.25f);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/CityManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityManager : MonoBehaviour {
+
+	public GameObject CityBlock;
+	public float width, height;
+
+	// Use this for initialization
+	void Start () {
+		for (float x = -width; x < width; x++) {
+			for (float y = -height; y < height; y++) {
+				GameObject CityObject = Instantiate<GameObject> (CityBlock);
+				CityObject.transform.SetParent (transform);
+				CityObject.transform.localPosition = new Vector3 (2f*x, 0f, 2f*y);
+				CityGenerator CG = CityObject.GetComponent<CityGenerator> ();
+				//City Design 1st Num is Spread
+				CG.TowerHealth = 45f*Mathf.Abs(((1f-(Mathf.Abs(x)/width))+(1f-(Mathf.Abs(y)/height)))/4f);
+				CG.GenerateCity();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
